5976cc487a198fab6d11db0f8258bca9ea563060
Nav_InverseInfinity <98486154+nav-InverseInfinity@users.noreply.github.com>
2022-09-12 09:22:03 +0100

    updated

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 <html>
 <body>
 
-<h2>My First JavaScript</h2>
+<h2>My second JavaScript</h2>
 
 <button type="button"
 onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
+Click here to display Date and Time.</button>
 
 <p id="demo"></p>
 
